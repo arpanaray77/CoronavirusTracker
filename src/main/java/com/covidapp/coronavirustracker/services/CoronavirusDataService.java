64c2f4f4a2e43051873c57d33a91f1937bd3cad6
arpanaray77 <arpanaray75897@gmail.com>
2021-04-30 00:14:33 +0530

    Enabled Scheduling for automatic update of Data

--- src/main/java/com/covidapp/coronavirustracker/services/CoronavirusDataService.java
+++ src/main/java/com/covidapp/coronavirustracker/services/CoronavirusDataService.java
@@ -1,11 +1,16 @@
 package com.covidapp.coronavirustracker.services;
 
 import java.io.IOException;
+import java.io.StringReader;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import javax.annotation.PostConstruct;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 
@@ -15,6 +20,7 @@ public class CoronavirusDataService {
 	public static String VIRUS_DATA_URL="https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv";
 	
 	@PostConstruct
+	@Scheduled(cron="******") //scheduled to run revery second
 	public void fetchVirusData() throws IOException, InterruptedException
 	{
 	  HttpClient client=HttpClient.newHttpClient();
@@ -23,7 +29,18 @@ public class CoronavirusDataService {
 			  			.build();
 	  
 	  HttpResponse<String> httpresponse= client.send(request,HttpResponse.BodyHandlers.ofString());
-	  System.out.println(httpresponse.body());
+	 // System.out.println(httpresponse.body());
+	  
+	  //getting csv file from string
+	   StringReader csvBodyHeader = new StringReader(httpresponse.body());
+	  Iterable<CSVRecord> records = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(csvBodyHeader);
+	  for (CSVRecord record : records) {
+	      String state = record.get("Province/State");
+	      System.out.println(state);
+//	      String customerNo = record.get("CustomerNo");
+//	      String nam = record.get("Name");
+	  }
+	          
 	}
 
 }
