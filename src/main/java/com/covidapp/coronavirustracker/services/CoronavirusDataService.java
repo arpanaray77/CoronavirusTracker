8e8a997d8c71dbea5efaafd0038c86b747e1af11
arpanaray77 <arpanaray75897@gmail.com>
2021-04-30 17:08:05 +0530

    Getting Capitals for Countries from csv file

--- src/main/java/com/covidapp/coronavirustracker/services/CoronavirusDataService.java
+++ src/main/java/com/covidapp/coronavirustracker/services/CoronavirusDataService.java
@@ -21,6 +21,7 @@ import org.springframework.stereotype.Service;
 public class CoronavirusDataService {
 	
 	public static String VIRUS_DATA_URL="https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv";
+	public static String COUNTRY_CAPITAL="https://raw.githubusercontent.com/icyrockcom/country-capitals/master/data/country-list.csv";
 	private List<LocationData> allStats =new ArrayList<>();
 	
 	public List<LocationData> getAllStats() {
@@ -46,20 +47,46 @@ public class CoronavirusDataService {
 	  HttpResponse<String> httpresponse= client.send(request,HttpResponse.BodyHandlers.ofString());
 	 // System.out.println(httpresponse.body());
 	  
+	  HttpRequest request2=HttpRequest.newBuilder()
+	  			.uri(URI.create(COUNTRY_CAPITAL))
+	  			.build();
+
+	HttpResponse<String> httpresponse2= client.send(request2,HttpResponse.BodyHandlers.ofString());
+	//System.out.println(httpresponse2.body());
+
+	  
 	  //getting csv file from string
 	   StringReader csvBodyHeader = new StringReader(httpresponse.body());
+	   StringReader countryName = new StringReader(httpresponse2.body());
+	   
 	  Iterable<CSVRecord> records = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(csvBodyHeader);
+	  Iterable<CSVRecord> countries = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(countryName);
+	  
 	  for (CSVRecord record : records) {
 		  
 		  LocationData data=new LocationData();
 		  if(record.get("Province/State").length()>0) 
 		      data.setState(record.get("Province/State"));
 		  else
-			  data.setState("Capital");
+		  {
+			  String countryname=record.get("Country/Region");
+			  boolean flag=false;
+			  for (CSVRecord country: countries)
+			  {
+				  if(country.get("country").equals(countryname))
+				  {
+					  flag =true;
+					 // System.out.println(country.get("country") +" "+country.get("capital"));
+					  data.setState(country.get("capital")); 
+					  break;
+				  }
+			  }
+			  if(!flag)
+				  data.setState("Capital");
+		  }
 		  data.setCountry(record.get("Country/Region"));
 		  data.setLatestTotalCases(Integer.parseInt(record.get(record.size()-1)));
 		  data.setDiffrenceFromPrevDay(Math.abs(Integer.parseInt(record.get(record.size()-2))-Integer.parseInt(record.get(record.size()-1))));
-	      //System.out.println(data.getCountry()+"  "+data.getLatestTotalCases());
           newStats.add(data);
 	  }
 	  this.allStats = newStats;
